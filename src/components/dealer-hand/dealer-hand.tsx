4cc53241ee241b5503476421e1c8f2997b639000
Deryx <deryxescott@gmail.com>
2023-06-08 22:52:27 -0400

    Blackjack-gatsby: Update dealer area with score and first card invisibility

--- src/components/dealer-hand/dealer-hand.tsx
+++ src/components/dealer-hand/dealer-hand.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import DealerHandProps from './props';
+import ranks from '../card/ranks';
 import './styles.scss';
 
 const Dealerhand = ({ hand }: DealerHandProps) => {
+    const handTotal = ( hand: any ): number => {
+        let total: number = 0;
+
+        for(const card of hand) {
+            total += ranks[card.props.rank]
+        }
+
+        return total;
+    }
+
     return (
-        <div className='cards'>
-            { hand }
-        </div>
+        <>
+            <div id='dealer-score' className='score'>
+                <label>score: </label>
+                { handTotal(hand) }
+            </div>
+            <div className='cards'>
+                { hand }
+            </div>
+        </>
     )
 };
 
