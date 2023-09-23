35b80fdeb058e9b15f45e6293d563bb4bbdc0d13
Deryx <deryxescott@gmail.com>
2023-09-23 19:53:55 -0400

    Update gatsby-config file

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,13 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `blackjack gatsby`,
+    title: `blackjack-gatsby`,
     siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  pathPrefix: "/blackjack-gatsby",
   plugins: ["gatsby-plugin-sass"]
 };
 
